8e629ce66eab397978ea9c1e8bf78b81f96d109d
Aleksey Klimov <ax.klim@gmail.com>
2022-10-26 21:13:10 +0300

    Switch between channels

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+const isMac = process.platform === 'darwin';
 
-let win;
+let mainWindow;
+
+const menuTemplate = [
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideOthers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+        ]
+    }] : []),
+    {
+        label: 'lofi',
+        submenu: [
+            {
+                label: 'beats to relax/study to',
+                accelerator: 'cmd+1',
+                click() {
+                    mainWindow.loadFile('index.html');
+                },
+            },
+            {
+                label: 'beats to sleep/chill to',
+                accelerator: 'cmd+2',
+                click() {
+                    mainWindow.loadFile('chill.html');
+                },
+            },
+            isMac ? { role: 'close' } : { role: 'quit' }
+        ]
+    },
+    { role: 'viewMenu' },
+    { role: 'windowMenu' },
+]
 
 const createWindow = () => {
-    win = new BrowserWindow({
+    return new BrowserWindow({
         backgroundColor: '#253238',
         show: false,
         frame: false,
@@ -11,19 +51,23 @@ const createWindow = () => {
         height: 810,
         resizable: false,
     });
-    win.loadFile('index.html');
-    win.once('ready-to-show', () => win.show());
 };
 
 app.on('ready', () => {
-    createWindow();
-    const wc = win.webContents;
+    mainWindow = createWindow();
+    mainWindow.loadFile('index.html');
+    mainWindow.once('ready-to-show', () => mainWindow.show());
+
+    const wc = mainWindow.webContents;
     wc.on('did-finish-load', () => {
         // autoplay hack :)
         wc.sendInputEvent({keyCode: 'Tab', type: 'keyDown'});
         wc.sendInputEvent({keyCode: 'Tab', type: 'char'});
         wc.sendInputEvent({keyCode: 'Tab', type: 'keyUp'});
     });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
 
 app.on('window-all-closed', () => {
